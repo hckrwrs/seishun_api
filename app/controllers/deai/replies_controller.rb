c0010de042af364d1c9425cbaed1ef483c2efe18
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-05-14 22:36:42 +0900

    (D)Reply するAPI

--- app/controllers/deai/replies_controller.rb
+++ app/controllers/deai/replies_controller.rb
@@ -1,2 +1,13 @@
 class Deai::RepliesController < ApplicationController
+  before_action :authenticate_deai_user!
+
+  def create
+    guchi_id = params.require(:guchi_id)
+    content = params.require(:content)
+    guchi = Guchi.find guchi_id
+
+    rep = Reply.create! guchi_id: guchi_id, deai_user_id: current_deai_user.id, guchi_user_id: guchi.guchi_user_id, is_guching: false, content: content
+
+    render_ok rep
+  end
 end
