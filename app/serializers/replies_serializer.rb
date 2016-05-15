bdc09b9ee2942f35ad315577bb357d252712f99f
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-05-15 11:37:12 +0900

    fix server

--- app/serializers/replies_serializer.rb
+++ app/serializers/replies_serializer.rb
@@ -2,7 +2,13 @@ class RepliesSerializer < ActiveModel::Serializer
   attributes :id, :content, :is_guching, :deai_user_id, :read, :guchi_user_icon_value
   belongs_to :guchi
   belongs_to :guchi_user
+  belongs_to :deai_user
+
   def guchi_user_icon_value
+    return object.guchi_user.icon.value
+  end
+
+  def deai_user_icon_value
     return object.guchi_user.icon.value
   end
 end
