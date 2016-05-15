94a63a13b48c7a21e22042cdbb944a2a104e8f62
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-05-15 09:22:52 +0900

    最新のリプライのみ返却

--- app/serializers/guchi_include_reply_serializer.rb
+++ app/serializers/guchi_include_reply_serializer.rb
@@ -1,4 +1,7 @@
 class GuchiIncludeReplySerializer < ActiveModel::Serializer
-  attributes :id, :content, :guchi_user_id
-  has_many :replies
+  attributes :id, :content, :guchi_user_id, :replies
+
+  def replies
+    object.replies.group_by{|x| x.deai_user_id}.map{|_, reps| reps.sort_by{|r| r.created_at}.last}
+  end
 end
